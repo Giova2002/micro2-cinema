08242212c50f85f6067980aef9f5040d09e707ec
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2023-06-11 22:39:51 -0400

    arregamos el buscar y nos la pela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 import React, { useState, useEffect } from "react";
 import styles from "./Home.module.css";
 
@@ -7,13 +6,14 @@ const Home = () => {
   const [pagina, setPagina] = useState(1);
   const [peliculas, setPeliculas] = useState([]);
   const [peliculaSeleccionada, setPeliculaSeleccionada] = useState(null);
+  const [busqueda, setBusqueda] = useState("");
+  const [resultados, setResultados] = useState([]);
 
   const cargarPeliculas = async (pagina) => {
     try {
       const respuesta = await fetch(
         `https://api.themoviedb.org/3/movie/popular?api_key=a14c80ae068cf2ee2e614b27391deec2&language=es-MX&page=${pagina}`
       );
-    //   https://api.themoviedb.org/3/movie/550?api_key=a14c80ae068cf2ee2e614b27391deec2
 
       console.log(respuesta);
 
@@ -53,7 +53,7 @@ const Home = () => {
       const respuesta = await fetch(
         `https://api.themoviedb.org/3/movie/${pelicula.id}?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&append_to_response=genres`
       );
-  
+
       if (respuesta.status === 200) {
         const datos = await respuesta.json();
         setPeliculaSeleccionada(datos);
@@ -73,6 +73,27 @@ const Home = () => {
     setPeliculaSeleccionada(null);
   };
 
+  const buscarPeliculas = async () => {
+    try {
+      const respuesta = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=a14c80ae068cf2ee2e614b27391deec2&language=es-MX&query=${busqueda}&page=1&include_adult=false`
+      );
+
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        setResultados(datos.results);
+      } else if (respuesta.status === 401) {
+        console.log("Pusiste la llave mal");
+      } else if (respuesta.status === 404) {
+        console.log("No se encontraron resultados");
+      } else {
+        console.log("Hubo un error y no sabemos que paso");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="hero">
@@ -87,69 +108,245 @@ const Home = () => {
       </div>
 
       <div className={styles.container}>
-        {/* <h2>Películas populares</h2> */}
+        <div className={styles.busqueda}>
+          <input
+            type="text"
+            value={busqueda}
+            onChange={(e) => setBusqueda(e.target.value)}
+          />
+          <button onClick={buscarPeliculas}>Buscar</button>
+        </div>
+
         <div className={styles.row}>
-          {peliculas.map((pelicula) => (
-            <div
-              className={styles.pelicula}
-              key={pelicula.id}
-              onClick={() => handlePeliculaSeleccionada(pelicula)}
-            >
-              <img
-                className={styles.poster}
-                src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
-                alt={pelicula.title}
-              />
-              <h3>{pelicula.title}</h3>
-            </div>
-          ))}
+          {resultados.length > 0
+            ? resultados.map((pelicula) => (
+                <div
+                  className={styles.pelicula}
+                  key={pelicula.id}
+                  onClick={() => handlePeliculaSeleccionada(pelicula)}
+                >
+                  <img
+                    className={styles.poster}
+                    src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+                    alt={pelicula.title}
+                  />
+                  <h3>{pelicula.title}</h3>
+                </div>
+              ))
+            : peliculas.map((pelicula) => (
+                <div
+                  className={styles.pelicula}
+                  key={pelicula.id}
+                  onClick={() => handlePeliculaSeleccionada(pelicula)}
+                >
+                  <img
+                    className={styles.poster}
+                    src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+                    alt={pelicula.title}
+                  />
+                  <h3>{pelicula.title}</h3>
+                </div>
+              ))}
         </div>
 
         {peliculaSeleccionada && (
           <div className={styles.modal}>
-          <div className={styles.modalContent}>
-            <button className={styles.closeButton} onClick={handlePeliculaDeseleccionada}>
-              X
-            </button>
-            <div className={styles.botones}>
+            <div className={styles.modalContent}>
+              <button
+                className={styles.closeButton}
+                onClick={handlePeliculaDeseleccionada}
+              >
+                X
+              </button>
+              <div className={styles.botones}>
                 <Link to="/Peliculas">
-                    <button>Ver Más</button>
+                  <button>Ver Más</button>
                 </Link>
+              </div>
+              <img
+                className={styles.poster}
+                src={`https://image.tmdb.org/t/p/w500/${peliculaSeleccionada.poster_path}`}
+                alt={peliculaSeleccionada.title}
+              />
+              <h3>{peliculaSeleccionada.title}</h3>
+              <h4>Idioma</h4>
+              <p>{peliculaSeleccionada.original_language}</p>
+              <h4>Género</h4>
+              <ul>
+                {peliculaSeleccionada.genres.map((genero) => (
+                  <li key={genero.id}>{genero.name}</li>
+                ))}
+              </ul>
             </div>
-            <img
-              className={styles.poster}
-              src={`https://image.tmdb.org/t/p/w500/${peliculaSeleccionada.poster_path}`}
-              alt={peliculaSeleccionada.title}
-            />
-            <h3>{peliculaSeleccionada.title}</h3>
-            {/* <p>{peliculaSeleccionada.overview}</p> */}
-            <h4>Idioma</h4>
-            <p>{peliculaSeleccionada.original_language}</p>
-            <h4>Género</h4>
-            <ul>
-              {peliculaSeleccionada.genres.map((genero) => (
-                <li key={genero.id}>{genero.name}</li>
-              ))}
-            </ul>
           </div>
-        </div>)}
+        )}
 
         <div className={styles.paginacion}>
           <button onClick={handleAnterior}>Anterior</button>
           <button onClick={handleSiguiente}>Siguiente</button>
         </div>
-        {/* <div className={styles.botones}>
-        <Link to="/Peliculas">
-            <button>Ir a Película</button>
-        </Link>
-     </div> */}
-      </div>
-      
-    </div>
-  );
-};
-
+        </div>
+        </div>
+        );
+    };
 export default Home;
+
+
+
+// import { Link } from "react-router-dom";
+
+// import React, { useState, useEffect } from "react";
+// import styles from "./Home.module.css";
+
+// const Home = () => {
+//   const [pagina, setPagina] = useState(1);
+//   const [peliculas, setPeliculas] = useState([]);
+//   const [peliculaSeleccionada, setPeliculaSeleccionada] = useState(null);
+
+//   const cargarPeliculas = async (pagina) => {
+//     try {
+//       const respuesta = await fetch(
+//         `https://api.themoviedb.org/3/movie/popular?api_key=a14c80ae068cf2ee2e614b27391deec2&language=es-MX&page=${pagina}`
+//       );
+//     //   https://api.themoviedb.org/3/movie/550?api_key=a14c80ae068cf2ee2e614b27391deec2
+
+//       console.log(respuesta);
+
+//       if (respuesta.status === 200) {
+//         const datos = await respuesta.json();
+//         setPeliculas(datos.results);
+//       } else if (respuesta.status === 401) {
+//         console.log("Pusiste la llave mal");
+//       } else if (respuesta.status === 404) {
+//         console.log("La pelicula que buscas no existe");
+//       } else {
+//         console.log("Hubo un error y no sabemos que paso");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     cargarPeliculas(pagina);
+//   }, [pagina]);
+
+//   const handleSiguiente = () => {
+//     if (pagina < 1000) {
+//       setPagina(pagina + 1);
+//     }
+//   };
+
+//   const handleAnterior = () => {
+//     if (pagina > 1) {
+//       setPagina(pagina - 1);
+//     }
+//   };
+
+//   const handlePeliculaSeleccionada = async (pelicula) => {
+//     try {
+//       const respuesta = await fetch(
+//         `https://api.themoviedb.org/3/movie/${pelicula.id}?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&append_to_response=genres`
+//       );
+  
+//       if (respuesta.status === 200) {
+//         const datos = await respuesta.json();
+//         setPeliculaSeleccionada(datos);
+//       } else if (respuesta.status === 401) {
+//         console.log("Pusiste la llave mal");
+//       } else if (respuesta.status === 404) {
+//         console.log("La pelicula que buscas no existe");
+//       } else {
+//         console.log("Hubo un error y no sabemos que paso");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const handlePeliculaDeseleccionada = () => {
+//     setPeliculaSeleccionada(null);
+//   };
+
+//   return (
+//     <div>
+//       <div className="hero">
+//         <div className="carrusel">
+//           <img src="foto/img1.jpg" alt="Imagen 1" />
+//           <img src="foto/img2.jpg" alt="Imagen 2" />
+//           <img src="foto/img3.jpg" alt="Imagen 3" />
+//           <img src="foto/img4.jpg" alt="Imagen 4" />
+//           <img src="foto/img5.jpg" alt="Imagen 5" />
+//           <img src="foto/img6.jpg" alt="Imagen 6" />
+//         </div>
+//       </div>
+
+
+
+//       <div className={styles.container}>
+//         {/* <h2>Películas populares</h2> */}
+//         <div className={styles.row}>
+//           {peliculas.map((pelicula) => (
+//             <div
+//               className={styles.pelicula}
+//               key={pelicula.id}
+//               onClick={() => handlePeliculaSeleccionada(pelicula)}
+//             >
+//               <img
+//                 className={styles.poster}
+//                 src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+//                 alt={pelicula.title}
+//               />
+//               <h3>{pelicula.title}</h3>
+//             </div>
+//           ))}
+//         </div>
+
+//         {peliculaSeleccionada && (
+//           <div className={styles.modal}>
+//           <div className={styles.modalContent}>
+//             <button className={styles.closeButton} onClick={handlePeliculaDeseleccionada}>
+//               X
+//             </button>
+//             <div className={styles.botones}>
+//                 <Link to="/Peliculas">
+//                     <button>Ver Más</button>
+//                 </Link>
+//             </div>
+//             <img
+//               className={styles.poster}
+//               src={`https://image.tmdb.org/t/p/w500/${peliculaSeleccionada.poster_path}`}
+//               alt={peliculaSeleccionada.title}
+//             />
+//             <h3>{peliculaSeleccionada.title}</h3>
+//             {/* <p>{peliculaSeleccionada.overview}</p> */}
+//             <h4>Idioma</h4>
+//             <p>{peliculaSeleccionada.original_language}</p>
+//             <h4>Género</h4>
+//             <ul>
+//               {peliculaSeleccionada.genres.map((genero) => (
+//                 <li key={genero.id}>{genero.name}</li>
+//               ))}
+//             </ul>
+//           </div>
+//         </div>)}
+
+//         <div className={styles.paginacion}>
+//           <button onClick={handleAnterior}>Anterior</button>
+//           <button onClick={handleSiguiente}>Siguiente</button>
+//         </div>
+//         {/* <div className={styles.botones}>
+//         <Link to="/Peliculas">
+//             <button>Ir a Película</button>
+//         </Link>
+//      </div> */}
+//       </div>
+      
+//     </div>
+//   );
+// };
+
+// export default Home;
 
 
 
